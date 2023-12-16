dcdc5c71aea80d212088747d8aac4ca7ba9f8e79
Hilla <hilla.abramov@gmail.com>
2023-12-16 21:53:18 +0000

    first commit script to send data to Kinesis streams

---
+++ user_posting_emulation_streaming.py
@@ -0,0 +1,29 @@
+# In this script, you should send requests to your API, which adds one record at a time to the streams you have created.
+# You should send data from the three Pinterest tables to their corresponding Kinesis stream.
+import random
+from time import sleep
+
+from aws_db_connector import AWSDBConnector
+from streaming_data_generator import StreamingDataGenerator
+
+
+new_connector = AWSDBConnector("aws_db_config.yaml")
+pin_data = StreamingDataGenerator("streaming-0a0223c10829-pin", "pinterest_data")
+geo_data = StreamingDataGenerator("streaming-0a0223c10829-geo", "geolocation_data", "timestamp")
+user_data = StreamingDataGenerator("streaming-0a0223c10829-user", "user_data", "date_joined")
+
+data_generators_by_stream = [pin_data, geo_data, user_data]
+
+random.seed(100)
+
+def run_infinite_post_data_to_kinesis_loop():
+    engine = new_connector.create_db_connector()
+    while True:
+        sleep(random.randrange(0, 2))
+        random_row_number = random.randint(0, 11000)
+        with engine.connect() as connection:
+            for data_generator in data_generators_by_stream:
+                data_generator.send_random_record_to_stream(connection, random_row_number)
+
+if __name__ == "__main__":
+    run_infinite_post_data_to_kinesis_loop()
