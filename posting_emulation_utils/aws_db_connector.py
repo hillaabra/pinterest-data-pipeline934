27036bae77e101b8e3cabb271fc77996d7c032cf
Hilla <hilla.abramov@gmail.com>
2023-12-24 15:16:42 +0000

    type hinting and docstrings

--- posting_emulation_utils/aws_db_connector.py
+++ posting_emulation_utils/aws_db_connector.py
@@ -1,10 +1,39 @@
 import yaml
 import sqlalchemy
 
+
 class AWSDBConnector:
+    """
+    Class providing connection to a MySQL database using SQLAlchemy and PyMySQL.
+    Used for this pipeline to provide connection to an RDS database on AWS storing
+    Pinterest user event data to be used for emulating real-world data being generated
+    by Pinterest users for ingestion into the pipeline.
 
-    def __init__(self, credentials_yaml):
+    Parameter:
+    --------
+    credentials_yaml: str
+        The filepath to the YAML file in the repository containing the required SQLAlchemy engine
+        creation settings.
 
+    Attributes:
+    ----------
+    HOST: str
+        The "HOST" setting loaded from credentials YAML file
+    USER: str
+        The "USER" setting loaded from the credentials YAML file
+    PASSWORD: str
+        The "PASSWORD" setting loaded from the credentials YAML file
+    DATABASE: str
+        The "DATABASE" setting loaded from the credentials YAML file
+    PORT: int
+        The "PORT" setting loaded from the credentials YAML file
+    """
+    def __init__(self, credentials_yaml: str) -> None:
+        """
+        Method to initialise the object's attributes by accessing the contents of
+        the credentials YAML file at the filepath passed into the contructor as
+        parameter.
+        """
         with open(credentials_yaml, 'r') as stream:
             dict_db_creds = yaml.safe_load(stream)
 
@@ -14,6 +43,13 @@ class AWSDBConnector:
         self.DATABASE = dict_db_creds["DATABASE"]
         self.PORT = dict_db_creds["PORT"]
 
-    def create_db_connector(self):
+    def create_db_connector(self) -> sqlalchemy.engine:
+        """
+        Method to create a SQLAlchemy Engine object for connecting to the remote database.
+
+        Returns:
+        -------
+        sqlalchemy.engine: a SQLAlchemy Engine object which can be used for connecting to the remote database.
+        """
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
