fe447b05f6a155105460320760e8ab4ea802c0e8
Hilla <hilla.abramov@gmail.com>
2023-12-24 17:11:26 +0000

    docstrings and type hinting

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,31 +1,63 @@
+import concurrent.futures
 import random
+
 from multiprocessing import Event, Process
 from time import sleep
 
 from posting_emulation_utils.aws_db_connector import AWSDBConnector
 from posting_emulation_utils.data_sender import DataSender
 
-import concurrent.futures
-
 
 new_connector = AWSDBConnector("posting_emulation_utils/aws_db_config.yaml")
 
-def worker(data_sender, stop_event, random_number_generator):
+def run_infinite_post_loop(data_sender: DataSender,
+                            stop_event: Event,
+                            random_number_generator: random.Random) -> None:
+    """
+    Function that sends a record at random, every 0-2 seconds, from the source table in the
+    remote AWS database to both the Stream Layer and Batch Layer of the pipeline concurrently.
+    The infinite loop is interrupted if an event is detected (the user is instucted to press
+    Enter to cease sending data to the pipeline).
 
+    Arguments:
+    ---------
+    data_sender: DataSender
+        An instance of the DataSender class which defines which table the data is being extracted
+        from and therefore which topic and stream the data is to be ingested through.
+
+    stop_event: Event
+        A multiprocessing.Event object which tells the infinite post loop to stop running if set
+        by the user's input.
+
+    random_number_generator: random.Random
+        A random number generator object initialised outside the function definition to ensure
+        parallelism in the time delay counts and index values of the rows of data being extracted
+        and ingested by each parallel infinite_post_loop process (i.e. for each dataset).
+    """
+
+    # establishes a SQLAlchemy engine object for connection to the RDS Database on AWS of pre-existing data
     engine = new_connector.create_db_connector()
 
+    # creates a thread pool in order to send data to the batch and stream layers concurrently
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as pool:
 
         while not stop_event.is_set():
 
+            # the random_row_number selected on each iteration of the loop is consistent across
+            # all processes (so each process extracts a row of data with N index - in total producing
+            # 3 datapoints relating to the same user posting event)
             random_row_number = random_number_generator.randint(0, 11000)
 
+            # extracts a row of data from the remote database, to emulate user posting data generation
             with engine.connect() as connection:
                 dict_for_api_request = data_sender.produce_data_dict_for_request_payload(connection, random_row_number)
 
+            # submits tasks to the thread pool executor
             pool.submit(data_sender.post_record_to_stream_layer, dict_for_api_request)
             pool.submit(data_sender.post_record_to_batch_layer, dict_for_api_request)
 
+            # there is a 0 to 2 second delay after each data posting event, which is synchronised
+            # between the processes thanks to the shared random number generator object parameter
             sleep(random_number_generator.randrange(0, 2))
 
 if __name__ == "__main__":
@@ -42,7 +74,7 @@ if __name__ == "__main__":
     processes = []
 
     for data_sender in data_senders:
-        process = Process(target=worker, args=(data_sender, stop_event, shared_random_number_generator))
+        process = Process(target=run_infinite_post_loop, args=(data_sender, stop_event, shared_random_number_generator))
         process.start()
         processes.append(process)
 
